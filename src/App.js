372e88353cf589f93a0b5a960efab4a00c16b19f
Adem Ottoman <50057725+Ademrobert@users.noreply.github.com>
2021-02-13 15:09:01 +0200

    Updated API key

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
 
   useEffect(() => {
     alanBtn({
-      key: '64370f4c903e66c5b517887fefa45c1b2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      key: '3f457fd4e8417202cd2392a597628e542e956eca572e1d8b807a3e2338fdd0dc/stage',
       onCommand: ({ command, articles, number }) => {
         if (command === 'newHeadlines') {
           setNewsArticles(articles);
